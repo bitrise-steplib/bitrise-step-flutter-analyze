4d0cf27b6ce3f540acaa62ed91d6df98a30f0780
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-07-16 17:10:24 +0200

    better test data

--- main_test.go
+++ main_test.go
@@ -17,13 +17,15 @@ func TestHasAnalyzeError(t *testing.T) {
 			info • Unused import: 'package:silkthread/service/ads_manager.dart' • lib/pages/post/image_edit.dart:8:8 • unused_import
 			info • Unused import: 'dart:ui' • lib/pages/post/image_full_view.dart:1:8 • unused_import
 			info • Unused import: 'package:cloud_firestore/cloud_firestore.dart' • lib/pages/post/image_full_view.dart:4:8 • unused_import
-			info • Unused import: 'package:font_awesome_flutter/font_awesome_flutter.dart' • lib/pages/post/image_full_view.dart:6:8 • unused_import`,
+			info • Unused import: 'package:font_awesome_flutter/font_awesome_flutter.dart' • lib/pages/post/image_full_view.dart:6:8 • unused_import
+			`,
 			false,
 		},
 		{
 			"contains error level violation",
 			`info • Unused import: 'dart:math' • lib/package.dart:3:8 • unused_import
-			error • Expected to find ';' • lib/package.dart:3:8 • expected_token`,
+			error • Expected to find ';' • lib/package.dart:3:8 • expected_token
+			`,
 			true,
 		},
 	}
